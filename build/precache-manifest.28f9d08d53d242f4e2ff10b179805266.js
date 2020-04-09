1d74db4322595845b071681bf421ed700ded7ff5
successful-lad <blessedonwebdev@gmail.com>
2020-04-09 17:38:30 +0300

    added too git

---
+++ build/precache-manifest.28f9d08d53d242f4e2ff10b179805266.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07f5cd96000b964eff93fa17798dee16",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b602204a75f5a6a8402f",
+    "url": "/static/css/main.d0ed0be9.chunk.css"
+  },
+  {
+    "revision": "865cdd142f9d8c9db442",
+    "url": "/static/js/2.cbf5f841.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.cbf5f841.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b602204a75f5a6a8402f",
+    "url": "/static/js/main.b07df964.chunk.js"
+  },
+  {
+    "revision": "d274380fe52123589ea6",
+    "url": "/static/js/runtime-main.3c1cf0ff.js"
+  },
+  {
+    "revision": "ab5c12ac17c8a708c71e04998f210e5f",
+    "url": "/static/media/deadMan.ab5c12ac.png"
+  },
+  {
+    "revision": "c0a33286db991eb23a17b4c6903a086e",
+    "url": "/static/media/lock.c0a33286.svg"
+  },
+  {
+    "revision": "4c4f7d24408f82ec04984664395fc1ad",
+    "url": "/static/media/mistake3.4c4f7d24.bin"
+  }
+]);
